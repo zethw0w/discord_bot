b9a245b4a0af8994adc64cf4b7fdad64aea1624e
zethw0w <155771007+zethw0w@users.noreply.github.com>
2024-10-29 18:03:41 -0400

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import discord
-import asyncio
 from discord.ext import commands
 import webserver
 
@@ -14,16 +13,17 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_message(message):
-    # Verifica se o autor da mensagem não é o bot e se o usuário mencionado é o ID desejado
+    
     if message.author.id != bot.user.id and MENTIONED_USER_ID in [user.id for user in message.mentions]:
         try:
-            for _ in range(58):
+            for _ in range(50):
                 await message.author.send("GET SPAMMED")
-                await asyncio.sleep(1)  # Adiciona um atraso de 1 segundo entre as mensagens
+                
         except discord.Forbidden:
             await message.channel.send(f"{message.author.mention}, tentei te enviar uma DM, mas não consegui. Por favor, habilite DMs.")
+    await bot.process_commands(message)
 
-# Inicia o bot
-bot.run(DISCORD_TOKEN)
 
 webserver.keep_alive()
+bot.run(DISCORD_TOKEN)
+
